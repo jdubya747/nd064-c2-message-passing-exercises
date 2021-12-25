ce1b67337dd06dab20f11796ec0d59312cd81e1c
jdubya747 <jdubya747@gmail.com>
2021-12-25 12:20:09 -0700

    Update app.py

--- lesson-3-implementing-message-passing/flask-starter/app.py
+++ lesson-3-implementing-message-passing/flask-starter/app.py
@@ -5,6 +5,14 @@ from .services import retrieve_orders, create_order
 
 app = Flask(__name__)
 
+@app.route('/api/orders/computers', methods=['GET', 'POST'])
+def computers():
+    if request.method == 'GET':
+        return jsonify(retrieve_orders())
+    elif request.method == 'POST':
+        return jsonify(create_orders(body=request.json))
+    else:
+        raise Exception('Unsupported HTTP request type.')
 
 @app.route('/health')
 def health():
