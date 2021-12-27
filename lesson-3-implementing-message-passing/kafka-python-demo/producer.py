f2f3c691a74c2a45be1e17ed80f80e75cd04b233
jdubya747 <jdubya747@gmail.com>
2021-12-27 12:02:22 -0700

    Update app.py

--- lesson-3-implementing-message-passing/kafka-python-demo/producer.py
+++ lesson-3-implementing-message-passing/kafka-python-demo/producer.py
@@ -1,10 +1,10 @@
 from kafka import KafkaProducer
 
 
-TOPIC_NAME = 'items'
+TOPIC_NAME = 'orders'
 KAFKA_SERVER = 'localhost:9092'
 
 producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER)
 
-producer.send(TOPIC_NAME, b'Test Message!!!')
+producer.send(TOPIC_NAME, b'{"created_at" : "2020-10-10"}')
 producer.flush()
